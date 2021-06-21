0e1e187a7d27ea67db53835ca87e20851570dfb4
Ivan <duremar98@yandex.ru>
2021-06-21 17:34:47 +0300

    jsx

--- src/components/panorama.tsx
+++ src/components/panorama.tsx
@@ -1,15 +1,39 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
-import { Content } from "../styles/style_panorama";
+import { Content, Search, Items, Item } from "../styles/style_panorama";
 import { useTypeSelector } from "../hooks/useTypeSelector";
 
 const Panorama: FC = () => {
+  const [opt, setOpt] = useState(true);
+  const [value, setValue] = useState("");
   const { respons715 } = useTypeSelector((state) => state.respons);
   const video = useTypeSelector((state) => state.video);
 
   return (
     <Content respons715={respons715} move={video.loading}>
-      Panorama
+      <Search>
+        <i
+          className="fas fa-caret-left fa-2x"
+          onClick={() => {
+            setOpt(!opt);
+            setValue("");
+          }}
+        ></i>
+        <input
+          type="text"
+          value={value}
+          placeholder={opt ? "film" : "director"}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <i
+          className="fas fa-caret-right fa-2x"
+          onClick={() => {
+            setOpt(!opt);
+            setValue("");
+          }}
+        ></i>
+      </Search>
+      <Items></Items>
     </Content>
   );
 };
--- src/styles/style_panorama.tsx
+++ src/styles/style_panorama.tsx
@@ -13,3 +13,23 @@ export const Content = styled.section<{ respons715: boolean; move: boolean }>`
     grid-column: span 4 / auto;
     `}
 `;
+
+export const Search = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 70px;
+  input {
+    height: 20px;
+    width: 160px;
+    padding: 3px 0 2px 2px;
+    margin: 0 3px;
+    font-size: 15px;
+    font-style: italic;
+  }
+  i {
+    cursor: pointer;
+  }
+`;
+export const Items = styled.div``;
+export const Item = styled.div``;
