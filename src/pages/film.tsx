5438158b2ad720085cbb3a290a385e7c215cfcfa
Ivan <duremar98@yandex.ru>
2021-06-25 20:44:18 +0300

    jsx

--- src/pages/film.tsx
+++ src/pages/film.tsx
@@ -1,6 +1,22 @@
 import React, { FC } from "react";
+import { useParams } from "react-router";
+
+import { useTypeSelector } from "../hooks/useTypeSelector";
 
 const Film: FC = () => {
+  const top = useTypeSelector((state) => state.top);
+  const panorama = useTypeSelector((state) => state.panorama);
+  let { id } = useParams<{ id: string }>();
+
+  // console.log([...panorama.items, ...top.items]);
+  // console.log(id);
+
+  const film = [...panorama.items, ...top.items].filter(
+    (item) => item.filmId === Number(id)
+  );
+
+  console.log(film);
+
   return <div>film</div>;
 };
 
